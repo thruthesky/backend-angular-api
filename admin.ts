b37eeafcb518bc031ff8d0dd1b5d6ef431bf723f
aysonsteven <aysonsteven@gmail.com>
2017-02-21 16:00:15 +0800

    initialize admin

---
+++ admin.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Base } from './base';
+import {  } from './interface';
+
+@Injectable()
+export class Admin{
+
+    constructor( private base: Base ){
+
+    }
+    
+
+}
